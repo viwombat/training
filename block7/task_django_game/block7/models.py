4f797320fd68db216130142aedac65b39c2cdd7e
viwombat <viktoriapobat@gmail.com>
2023-01-27 00:18:31 +0100

    add relationships

--- block7/task_django_game/block7/models.py
+++ block7/task_django_game/block7/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 
 class Publisher(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.PositiveIntegerField(primary_key=True)
     name = models.CharField(max_length=50)
 
 
 class Game(models.Model):
-    id = models.IntegerField(unique=True, primary_key=True)
+    id = models.PositiveIntegerField(unique=True, primary_key=True)
     name = models.CharField(max_length=50)
     price = models.FloatField()
     release_date = models.DateField()
-    # publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, null=True)
 
 
 class User(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.PositiveIntegerField(primary_key=True)
     nickname = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
     age = models.IntegerField()
-    # games = models.ManyToManyField(Game, on_delete=models.CASCADE)
+    games = models.ManyToManyField(Game)
 
 
 class Genre(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.PositiveIntegerField(primary_key=True)
     name = models.CharField(max_length=50)
-    # games = models.ManyToManyField(Game, on_delete=models.CASCADE)
+    games = models.ManyToManyField(Game)
